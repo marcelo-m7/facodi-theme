a7177ef75a6cc7bec7b7701d84decaac93015f0f
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 00:58:11 +0100

    refactor: migrate FACODI addon to theme_facodi

---
+++ theme_facodi/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "FACODI Theme",
+    "summary": "FACODI visual identity for Odoo Website and eLearning",
+    "version": "19.0.2.0.0",
+    "category": "Theme/Education",
+    "sequence": 120,
+    "author": "FACODI",
+    "website": "https://facodi.pt",
+    "license": "LGPL-3",
+    "depends": ["theme_common", "website_slides"],
+    "data": [
+        "data/generate_primary_template.xml",
+        "data/ir_asset.xml",
+        "views/customizations.xml",
+        "views/snippets.xml",
+        "views/website_slides.xml",
+    ],
+    "images": ["static/description/theme_facodi.svg"],
+    "images_preview_theme": {},
+    "configurator_snippets": {
+        "homepage": [
+            "s_facodi_hero",
+            "s_facodi_learning_journey",
+            "s_facodi_institutional",
+            "s_features",
+            "s_call_to_action",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
