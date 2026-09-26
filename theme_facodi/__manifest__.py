e011f0fd1a8cfe51a642ef4fe48cde5eed238e64
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-26 17:13:36 +0100

    chore(theme): release enriched surfaces 19.0.10.3.0

--- theme_facodi/__manifest__.py
+++ theme_facodi/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "FACODI Theme",
     "summary": "FACODI visual identity for Odoo Website and eLearning",
-    "version": "19.0.10.2.0",
+    "version": "19.0.10.3.0",
     "category": "Theme/Education",
     "sequence": 120,
     "author": "FACODI",
@@ -70,6 +70,7 @@
             "theme_facodi/static/src/scss/learning_interfaces.scss",
             "theme_facodi/static/src/scss/components.scss",
             "theme_facodi/static/src/scss/odoo_surfaces.scss",
+            "theme_facodi/static/src/scss/enriched_surfaces.scss",
             "theme_facodi/static/src/scss/website.scss",
             "theme_facodi/static/src/scss/snippets.scss",
             "theme_facodi/static/src/scss/foundation_v2.scss",
